4c2a50ec8e2e8a82112ef338765ee9f507f59abf
cristian mosquera <cmosqbone1@gmail.com>
2021-08-02 18:58:53 +0200

    add header html

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,11 +1,26 @@
+import logo from "../assets/images/logo.svg";
+
 const Header = () => {
     const view = `
-
-        <h1>Header componentes</h1>
-
-    `;
-
+                <div class="container">
+                    <nav>
+                        <div class="navbar">
+                            <div class="logo">
+                                <i class="fas fa-ticket-alt logo__title"></i>
+                                <span class="logo__name">SEARCH MOVIE</span>
+                            </div>
+                            <img src=${logo} class="navbar__img" alt="by the movie DB">
+                        </div>
+                    </nav>
+                </div>
+                <div class="hero">
+                    <div class="hero__content container">
+                        <h2 class="hero__content__title">Invasion: End of time</h2>
+                        <p class="hero__content__description">After the arrival of the aliens, Yuliya must face them, being the only one who had previous contact with them. Can loyalty be made stronger than power by alien technology?
+                        </p>
+                    </div>
+                </div>
+`;
     return view;
 };
-
 export default Header;
