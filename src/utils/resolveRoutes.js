408d7e325f62e6a68dc66e5d4b066ad173e235f4
cristian mosquera <cmosqbone1@gmail.com>
2021-08-03 08:51:42 +0200

    add icons

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,10 +1,11 @@
 const resolvesRoutes = (route) => {
-    if (route.length <= 3) {
-        let validRoute = route === "/" ? route : "/:id";
-        return validRoute;
-    }
-    return `/${route}`;
-    /about/;
+  if (route.length <= 3) {
+    let validRoute = route === "/" ? route : "/:id";
+
+    return validRoute;
+  }
+  return `/${route}`;
+  /about/;
 };
 
 export default resolvesRoutes;
